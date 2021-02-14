ee874d4e9429c11d9d1d3bd802ee744ab547f111
emily <emachieng00@gmail.com>
2021-02-15 01:58:18 +0900

    person getters

--- src/main/java/com/example/demo/model/Person.java
+++ src/main/java/com/example/demo/model/Person.java
@@ -1,5 +1,21 @@
 package com.example.demo.model;
+import java.util.UUID;
 
 public class Person {
+    private final UUID id;
+    private final string name;
+
+    public Person(UUID id, string name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public string getName() {
+        return  name;
+    }
 
 }
